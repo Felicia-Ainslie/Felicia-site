d341b5796d793c03f16cd258f240a73f57db12cf
Felicia-Ainslie <feliciainsights@gmail.com>
2026-06-05 23:31:42 -0500

    Card refactor & card test added

---
+++ components/ui/tests/Card.test.tsx
@@ -0,0 +1,29 @@
+//Test for ButtonLink component to ensure it renders correctly and has the correct href attribute.
+
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Card from "../Card";
+
+describe("Card", () => {
+    it("renders card title", () => {
+        render(
+        <Card title="Test Card">
+            <p>Test Content</p>
+        </Card>
+        );
+
+        expect(
+            screen.getByRole("heading", { name: "Test Card" })
+        ).toBeInTheDocument();
+    });
+
+    it("renders card content", () => {
+        render(
+        <Card title="Test Card">
+            <p>Test Content</p>
+        </Card>
+        );
+        
+        expect(screen.getByText(/Test Content/i)).toBeInTheDocument();
+    });
+});
